a7c0218075a09f4c205ef584f316a613598f1b09
CrazyVideoGamer <crazyvideogamez.help@gmail.com>
2021-09-24 22:49:42 -0400

    separate cli and main function

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-console.log("---------------------") // separater for nodemon dev
-
-let argv = process.argv.slice(2);
-if (argv.length < 1) {
-  console.error("Please give the file or folder to share.");
-} else if (argv.length > 2) {
-  console.error("Too much arguments. Please provide only one argument.")
-} else if (argv.includes("--help") || argv.includes("-h")) {
-  console.log(`shareIt - an easy way to transfer files/folders to different devices
-
-Usage: shareIt ./path/to/file/or/folder
-
-Options:
-  -h or --help    Display help message and exit
-`)
-} else {
+function share(target, port) {
 
 const fs = require("fs");
 const path = require("path");
-
-let target = argv[0];
-if (!path.isAbsolute(target)) {
-  target = path.join(process.cwd(), target);
-}
 
 let stats = undefined;
 
@@ -110,4 +90,9 @@ try {
   console.log(e);
 }
 
+}
+
+
+module.exports = {
+	share: share
 }
